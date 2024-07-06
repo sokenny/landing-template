e7b12bb08327f39471aff9d46ab065ab82aa042e
Juan Chaher <juanchaher99@gmail.com>
2024-07-06 19:13:10 -0300

    remove api key

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,6 @@ export default function RootLayout({
     <html lang="en" className="light">
      
       <head>
-      <script async src="http://localhost:3001/public/clientjs" data-stellar-api-key="d1e450b0c6d703bb36363e7803e49272:6e36fe6f9240323538d6c6445ac0b6dac8e05f17f4cc90b2f8dfe75a209dcbe1"></script>
       </head>
 
       <body
