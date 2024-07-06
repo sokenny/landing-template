ae12666423bf4db9bec0da31f1f53eb3b0addcf3
Juan Chaher <juanchaher99@gmail.com>
2024-07-06 19:34:13 -0300

    update snippet

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
     <html lang="en" className="light">
      
       <head>
+      <script async src="http://localhost:3001/public/clientjs" data-stellar-api-key="4a62c2931c817711710501872333d150:21edb2f000b4042a4b7206a9c6d73e03e94985d85ca5610058af207183879390"></script>
       </head>
 
       <body
