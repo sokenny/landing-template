d1994402fc4c4ed21ba5e4376c8580713407765b
Juan Chaher <juanchaher99@gmail.com>
2024-07-14 13:02:49 -0300

    remove all js

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,11 +18,6 @@ export default function RootLayout({
     <html lang="en" className="light">
      
       <head>
-<link rel="preconnect" href="https://d3niuqph2rteir.cloudfront.net"/>
-<link rel="preconnect" href="https://d3niuqph2rteir.cloudfront.net"/>
-<link rel="dns-prefetch" href="https://d3niuqph2rteir.cloudfront.net"/>
-<link rel="preload" href="https://d3niuqph2rteir.cloudfront.net/client_js/stellar.js" as="script"/>
-<script async src="https://d3niuqph2rteir.cloudfront.net/client_js/stellar.js" data-stellar-api-key="a299fd2af47e1080dadc2b7a7de31106:0ca394cf79edd1a9e65c4c92677254d3c1ba54f7c5afe97a95ffa0cd0c75b3cb"/>
 
       </head>
 
