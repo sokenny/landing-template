1128303d05a7b5bb7f99d56c6a8e27ce2918880e
Juan Chaher <48157442+sokenny@users.noreply.github.com>
2024-07-18 20:50:18 -0300

    ad script

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,12 @@ export default function RootLayout({
      
       <head>
 
+<link rel="preconnect" href="http://localhost:3001"/>
+<link rel="preconnect" href="http://localhost:3001"/>
+<link rel="dns-prefetch" href="http://localhost:3001"/>
+<link rel="preload" href="http://localhost:3001/client_js/stellar.js" as="script"/>
+<script async src="http://localhost:3001/client_js/stellar.js" data-stellar-api-key="c5f5306173071fdf7d1bd7f97c0c5343:ceb5fcc639522df90b0984d495aa79508161a7eb27ab93ed1df90717c0788a8e"/>
+
       </head>
 
       <body
