6f01697187f0bc428fda79af6259ca79e7a081fc
Juan Chaher <juanchaher99@gmail.com>
2024-07-22 21:58:49 -0300

    new script

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,11 +19,12 @@ export default function RootLayout({
      
       <head>
 
+  
 <link rel="preconnect" href="https://d3niuqph2rteir.cloudfront.net"/>
 <link rel="preconnect" href="https://d3niuqph2rteir.cloudfront.net"/>
 <link rel="dns-prefetch" href="https://d3niuqph2rteir.cloudfront.net"/>
 <link rel="preload" href="https://d3niuqph2rteir.cloudfront.net/client_js/stellar.js" as="script"/>
-<script async src="https://d3niuqph2rteir.cloudfront.net/client_js/stellar.js" data-stellar-api-key="9801691ee7afaf634eb6597d21e20324:d15be932f33816830e610450c2c23e20274b3c4e3f409631eda2a365a12e5635"/>
+<script async src="https://d3niuqph2rteir.cloudfront.net/client_js/stellar.js" data-stellar-api-key="6aec476016af275c4def719526cddfc9:32d6c214b878909c70644ee95281bd30c39a620ceb9e970f661b3c476528d38a"/>
 
       </head>
 
