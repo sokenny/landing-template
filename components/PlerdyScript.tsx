4affd2ffc32292a972ecea5dd073fcca8791a2a9
Juan Chaher <juanchaher99@gmail.com>
2024-10-19 14:31:25 -0300

    install plerdy test

--- components/PlerdyScript.tsx
+++ components/PlerdyScript.tsx
@@ -11,21 +11,13 @@ const PlerdyScript = () => {
     mainScript.dataset.plerdy_code = '1';
 
     mainScript.innerHTML = `
-      var _protocol = document.location.protocol === "https:" ? "https://" : "http://";
-      _site_hash_code = "836ebce148d97995baf59010921a99ba", _suid = 55358;
-      var plerdyScript = document.createElement("script");
-      plerdyScript.setAttribute("defer", "");
-      plerdyScript.dataset.plerdymainscript = "plerdymainscript";
-      plerdyScript.src = "https://a.plerdy.com/public/js/click/main.js?v=" + Math.random();
-      var plerdymainscript = document.querySelector("[data-plerdymainscript='plerdymainscript']");
-      if (plerdymainscript) {
-        plerdymainscript.parentNode.removeChild(plerdymainscript);
-      }
-      try {
-        document.head.appendChild(plerdyScript);
-      } catch (t) {
-        console.log(t, "unable to add script tag");
-      }
+      var _protocol="https:"==document.location.protocol?"https://":"http://";
+    _site_hash_code = "836ebce148d97995baf59010921a99ba",_suid=55358, plerdyScript=document.createElement("script");
+    plerdyScript.setAttribute("defer",""),plerdyScript.dataset.plerdymainscript="plerdymainscript",
+    plerdyScript.src="https://a.plerdy.com/public/js/click/main.js?v="+Math.random();
+    var plerdymainscript=document.querySelector("[data-plerdymainscript='plerdymainscript']");
+    plerdymainscript&&plerdymainscript.parentNode.removeChild(plerdymainscript);
+    try{document.head.appendChild(plerdyScript)}catch(t){console.log(t,"unable add script tag")}
     `;
 
     document.head.appendChild(mainScript);
@@ -33,7 +25,7 @@ const PlerdyScript = () => {
     // A/B testing inline script
     const abTestingInlineScript = document.createElement('script');
     abTestingInlineScript.type = 'text/javascript';
-    abTestingInlineScript.innerHTML = 'var _suid = 55358;';
+    abTestingInlineScript.innerHTML = 'var _suid=55358;';
     document.head.appendChild(abTestingInlineScript);
 
     // A/B testing external script
