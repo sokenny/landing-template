09837968b2993edbac88e0fabe04c10e1b219ee6
Juan Chaher <48157442+sokenny@users.noreply.github.com>
2025-03-02 21:39:17 -0300

    add new snip

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,11 +44,18 @@ export default function RootLayout({
             `,
         }}
       />  
+  {/* <link rel="preconnect" href="https://d3niuqph2rteir.cloudfront.net" /> */}
+{/* <link rel="dns-prefetch" href="https://d3niuqph2rteir.cloudfront.net" /> */}
+{/* <script async src="https://d3niuqph2rteir.cloudfront.net/client_js/stellar.js?apiKey=d102e03a2c2ae7d23117c46abff4e70d:b62874df9ce5c4e41fe1b8ed6980bce18956cd3fc541fa2acddc24766652aa5e"></script> */}
+
+{/* <script async src="http://localhost:3001/public/clientjs?apiKey=efd8d5c46ebba908b3d937aa8c620905:2e696ce42ada9f662f76270be300f49991cb2423389b0bef9159b57a56c5bf86"></script> */}
+ 
+
   <link rel="preconnect" href="https://d3niuqph2rteir.cloudfront.net" />
 <link rel="dns-prefetch" href="https://d3niuqph2rteir.cloudfront.net" />
-<script async src="https://d3niuqph2rteir.cloudfront.net/client_js/stellar.js?apiKey=d102e03a2c2ae7d23117c46abff4e70d:b62874df9ce5c4e41fe1b8ed6980bce18956cd3fc541fa2acddc24766652aa5e"></script>
+<script async src="https://d3niuqph2rteir.cloudfront.net/client_js/stellar.js?apiKey=dfd454441f660e6f655ca6382900cd46:b60d3569452f7cdf5f9cbeecded893770e809bab065c32241ec60fe9f67054ec"></script>
+  </head>
 
- </head>
 
       <body
         className={cn(
