865d3ef9469c4916191364e7ee17c30bd7d1a695
Juan Chaher <juanchaher99@gmail.com>
2025-04-02 23:33:49 -0300

    update script

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,28 +22,19 @@ export default function RootLayout({
 
      
 
-        
-  {/* <script
-        type="text/javascript"
-        dangerouslySetInnerHTML={{
-          __html: `
-!function(){var e="body {opacity: 0 !important;}",t=document.createElement("style");t.type="text/css",t.id="page-hide-style",t.styleSheet?t.styleSheet.cssText=e:t.appendChild(document.createTextNode(e)),document.head.appendChild(t),window.rmo=function(){var e=document.getElementById("page-hide-style");e&&(e.parentNode.removeChild(e),document.body.style.opacity="")},setTimeout(window.rmo,3e3)}();
-            `,
-        }}
-      />  
-  <link rel="preconnect" href="https://d3niuqph2rteir.cloudfront.net" />
-<link rel="dns-prefetch" href="https://d3niuqph2rteir.cloudfront.net" /> */}
-{/* <script async src="http://localhost:3001/public/clientjs?apiKey=efd8d5c46ebba908b3d937aa8c620905:2e696ce42ada9f662f76270be300f49991cb2423389b0bef9159b57a56c5bf86"></script> */}
- 
- 
+      
 <script
-        type="text/javascript"
-        dangerouslySetInnerHTML={{
-          __html: `
+type="text/javascript"
+dangerouslySetInnerHTML={{
+  __html: `
 !function(){var e="body {opacity: 0 !important;}",t=document.createElement("style");t.type="text/css",t.id="page-hide-style",t.styleSheet?t.styleSheet.cssText=e:t.appendChild(document.createTextNode(e)),document.head.appendChild(t),window.rmo=function(){var e=document.getElementById("page-hide-style");e&&(e.parentNode.removeChild(e),document.body.style.opacity="")},setTimeout(window.rmo,3e3)}();
-            `,
-        }}
-      />  
+    `,
+}}
+/>  
+<link rel="preconnect" href="https://d3niuqph2rteir.cloudfront.net" />
+<link rel="dns-prefetch" href="https://d3niuqph2rteir.cloudfront.net" />
+<script async src="https://d3niuqph2rteir.cloudfront.net/client_js/stellar.js?apiKey=29760eb8d05b14779a48eb98a6b330dc:65f2a8bfd7395da4025368b1a026a9ce8642c0f0ffaa22110190297ba918105b"></script>
+
   {/* <link rel="preconnect" href="https://d3niuqph2rteir.cloudfront.net" /> */}
 {/* <link rel="dns-prefetch" href="https://d3niuqph2rteir.cloudfront.net" /> */}
 {/* <script async src="https://d3niuqph2rteir.cloudfront.net/client_js/stellar.js?apiKey=d102e03a2c2ae7d23117c46abff4e70d:b62874df9ce5c4e41fe1b8ed6980bce18956cd3fc541fa2acddc24766652aa5e"></script> */}
@@ -51,9 +42,7 @@ export default function RootLayout({
 {/* <script async src="http://localhost:3001/public/clientjs?apiKey=efd8d5c46ebba908b3d937aa8c620905:2e696ce42ada9f662f76270be300f49991cb2423389b0bef9159b57a56c5bf86"></script> */}
  
 
-  <link rel="preconnect" href="https://d3niuqph2rteir.cloudfront.net" />
-<link rel="dns-prefetch" href="https://d3niuqph2rteir.cloudfront.net" />
-<script async src="https://d3niuqph2rteir.cloudfront.net/client_js/stellar.js?apiKey=29760eb8d05b14779a48eb98a6b330dc:65f2a8bfd7395da4025368b1a026a9ce8642c0f0ffaa22110190297ba918105b"></script>
+  
   </head>
 
 
